8a10498409a195e576bdd76ff07c0e9178254513
Dimitar Donkov <m17k018@gmail.com>
2023-12-20 15:09:28 +0200

    Add enemies, collisions and end of game

--- main.py
+++ main.py
@@ -14,30 +14,53 @@ PLAYER_WIDTH = 50
 PLAYER_HEIGHT = 50
 PLAYER_VEL = 5
 
+ENEMY_WIDTH = 25
+ENEMY_HEIGHT = 25
+ENEMY_VEL = 5
+
 FONT = pygame.font.SysFont("arial", 30)
 
-def draw(player, elapsed_time):
+def draw(player, elapsed_time, enemies):
     WIN.blit(BACKGROUND, (0, 0))
 
     time_text = FONT.render(f"Time:{round(elapsed_time)}s", 1, (0, 0, 0))
+    WIN.blit(time_text, (10, 10))
 
     pygame.draw.rect(WIN, (255, 0, 0), player)
-    WIN.blit(time_text, (10, 10))
+
+    for enemy in enemies:
+        pygame.draw.rect(WIN, (255, 255, 255), enemy)
 
     pygame.display.update()
 
 def main():
     run = True
 
-    player = pygame.Rect(WIDTH // 2, HEIGHT // 2, PLAYER_WIDTH, PLAYER_HEIGHT)
+    player = pygame.Rect(WIDTH / 2 - PLAYER_WIDTH / 2, HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT)
 
     clock = pygame.time.Clock()
     start_time = time.time()
     elapsed_time = 0
 
+    enemy_add_increment = 2000 # miliseconds
+    enemy_count = 0
+    enemies = []
+    hit = False
+
     while run:
-        clock.tick(60) # delay the while loop to run maximum 60fps
+        # clock tick - return miliseconds since the last clock tick
+        enemy_count += clock.tick(60) # delay the while loop to run maximum 60fps
         elapsed_time = time.time() - start_time
+
+        # add enemies
+        if enemy_count > enemy_add_increment:
+            for _ in range(3):
+                enemy_x = random.randint(0, WIDTH - ENEMY_WIDTH)
+                enemy = pygame.Rect(enemy_x, -ENEMY_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT)
+                enemies.append(enemy)
+            
+            enemy_add_increment = max(200, enemy_add_increment - 50)
+            enemy_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -54,7 +77,23 @@ def main():
         if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
             player.y += PLAYER_VEL
 
-        draw(player, elapsed_time)
+        for enemy in enemies[:]:
+            enemy.y += ENEMY_VEL
+            if enemy.y > HEIGHT:
+                enemies.remove(enemy)
+            elif enemy.y + enemy.height >= player.y and enemy. colliderect(player):
+                enemies.remove(enemy)
+                hit = True
+                break
+
+        if hit:
+            text = FONT.render("You lose", 1, (0, 0, 0))
+            WIN.blit(text, (WIDTH / 2 - text.get_width() / 2, HEIGHT / 2 - text.get_height() / 2))
+            pygame.display.update()
+            pygame.time.delay(5_000)
+            break
+
+        draw(player, elapsed_time, enemies)
 
     pygame.quit()
 
